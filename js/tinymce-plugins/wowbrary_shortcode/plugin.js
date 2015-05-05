1582926501d76f2ed8d346a331d71d88ef0241f4
bkalish <bkalish@gmail.com>
2015-05-05 16:41:29 -0400

    completed wowbrary form and enabled double-click behavior

--- js/tinymce-plugins/wowbrary_shortcode/plugin.js
+++ js/tinymce-plugins/wowbrary_shortcode/plugin.js
@@ -28,6 +28,13 @@ function wowbraryShortcodePlugin(editor, url) {
 		stateSelector: 'div.wowbraryShortcodeGUI'
 	});
 
+	editor.on('dblclick', function(e) {
+		if (tinymce.DOM.is(e.target, '.wowbraryShortcodeGUI, .wowbraryShortcodeGUI *')) {
+			editor.execCommand('Wowbrary');
+			e.stopPropagation();
+		}
+	});
+
 	// replace shortcode before editor content is set
 	editor.on('BeforeSetContent', function(e) {
 		e.content = self._do_wowbrary_shortcode(e.content);
@@ -66,6 +73,7 @@ function wowbraryShortcodePlugin(editor, url) {
 
 		var win = tinymce.activeEditor.windowManager.open({
 			type: 'window',
+			title: 'Wowbrary Widget Edit',
 			bodyType: 'tabpanel',
 			onSubmit: function() {
 				var shortcode = '[wowbrary';
@@ -86,23 +94,7 @@ function wowbraryShortcodePlugin(editor, url) {
 					}
 				});
 			},
-			body:	[
-				{
-					title: 'Size',
-					type: 'form',
-					items: wowbraryFormData.sizeItems
-				},
-				{
-					title: 'Appearance',
-					type: 'form',
-					items: wowbraryFormData.appearanceItems
-				},
-				{
-					title: 'Other',
-					type: 'form',
-					items: wowbraryFormData.otherItems
-				}
-			]
+			body: wowbraryFormData
 		});
 	}
 
@@ -190,68 +182,125 @@ function wowbraryShortcodePlugin(editor, url) {
 
 }
 
-var wowbraryFormData = {
-	sizeItems: [
-		{type: 'textbox', name: 'height', size: 40, label: 'Height'},
-		{type: 'textbox', name: 'width', size: 40, label: 'Max Width'},
-		{type: 'textbox', name: 'container', size: 40, label: 'Selector'},
-		{type: 'label', text: '(If selector is set, the widget will be resized to match the size of the selected element.)'}
-	],
-	appearanceItems: [
-		{type: 'listbox', name: 'motion', label: 'Motion', values: [
-			{text: 'One at a time', value: 'one-at-a-time'},
-			{text: 'Continuous', value: 'continuous'},
-			{text: 'All at once', value: 'all-at-once'},
-			{text: 'Morph', value: 'morph'}
-		]},
-		{type: 'listbox', name: 'imagesize', label: 'Image Size', values: [
-			{text: 'Small', value: 'S'},
-			{text: 'Medium', value: 'M'},
-			{text: 'Large', value: 'L'}
-		]},
-		{type: 'textbox', name: 'imagescale', size: 40, label: 'Image Scale (%)'},
-		{type: 'textbox', name: 'reflectionsize', size: 40, label: 'Reflection Size (%)'},
-		{type: 'textbox', name: 'spacing', size: 40, label: 'Spacing'},
-		{type: 'textbox', name: 'switchtime', size: 40, label: 'Switch Time (ms)'},
-		{type: 'textbox', name: 'pause', size: 40, label: 'Pause Time (ms)'},
-		{type: 'checkbox', name: 'switchonlyonclick', text: 'Switch only on click?', label: ' '}
-	],
-	otherItems: [
-		{type: 'checkbox', name: 'noelectronic', text: 'Block electronic?', label: ' '},
-		{type: 'listbox', name: 'category', label: 'Category', values: [
-			{value: 'GEN', text: 'Top Choices'},
-			{value: 'ART', text: 'Arts & Photography'},
-			{value: 'BIO', text: 'Biographies & Memoirs'},
-			{value: 'BUS', text: 'Business & Investing'},
-			{value: 'CDS', text: 'CDs: Music & Shows'},
-			{value: 'CHI', text: 'Business & Investing'},
-			{value: 'CGR', text: 'Comics & Graphic Novels'},
-			{value: 'COM', text: 'Computers & Internet'},
-			{value: 'COO', text: 'Cooking, Food & Wine'},
-			{value: 'DVD', text: 'DVDs'},
-			{value: 'EBO', text: 'eBooks & eAudios'},
-			{value: 'ENT', text: 'Entertainment'},
-			{value: 'HEA', text: 'Health, Mind & Body'},
-			{value: 'HIS', text: 'History'},
-			{value: 'HOM', text: 'Home & Garden'},
-			{value: 'LRG', text: 'Large Print'},
-			{value: 'LIT', text: 'Literature & Fiction'},
-			{value: 'MYS', text: 'Mysteries & Thrillers'},
-			{value: 'NEN', text: 'Non-English'},
-			{value: 'OUT', text: 'Outdoors & Nature'},
-			{value: 'PAR', text: 'Parenting & Family'},
-			{value: 'PRO', text: 'Professional & Technical'},
-			{value: 'REF', text: 'Reference'},
-			{value: 'REL', text: 'Religion & Spirituality'},
-			{value: 'ROM', text: 'Romance'},
-			{value: 'SCI', text: 'Science'},
-			{value: 'SFF', text: 'Science Fiction & Fantasy'},
-			{value: 'SOC', text: 'Society'},
-			{value: 'SPO', text: 'Sports'},
-			{value: 'TEE', text: 'Teen'},
-			{value: 'TRA', text: 'Travel'}
-		]},
-		{type: 'textbox', name: 'count', size: 40, label: 'Max Count'}
-	]
-
-};
+var wowbraryFormData = [
+	{
+		title: 'Content',
+		type: 'form',
+		items: [
+			{type: 'listbox', name: 'category', label: 'Category', values: [
+				{value: 'GEN', text: 'Top Choices'},
+				{value: 'ART', text: 'Arts & Photography'},
+				{value: 'BIO', text: 'Biographies & Memoirs'},
+				{value: 'BUS', text: 'Business & Investing'},
+				{value: 'CDS', text: 'CDs: Music & Shows'},
+				{value: 'CHI', text: 'Business & Investing'},
+				{value: 'CGR', text: 'Comics & Graphic Novels'},
+				{value: 'COM', text: 'Computers & Internet'},
+				{value: 'COO', text: 'Cooking, Food & Wine'},
+				{value: 'DVD', text: 'DVDs'},
+				{value: 'EBO', text: 'eBooks & eAudios'},
+				{value: 'ENT', text: 'Entertainment'},
+				{value: 'HEA', text: 'Health, Mind & Body'},
+				{value: 'HIS', text: 'History'},
+				{value: 'HOM', text: 'Home & Garden'},
+				{value: 'LRG', text: 'Large Print'},
+				{value: 'LIT', text: 'Literature & Fiction'},
+				{value: 'MYS', text: 'Mysteries & Thrillers'},
+				{value: 'NEN', text: 'Non-English'},
+				{value: 'OUT', text: 'Outdoors & Nature'},
+				{value: 'PAR', text: 'Parenting & Family'},
+				{value: 'PRO', text: 'Professional & Technical'},
+				{value: 'REF', text: 'Reference'},
+				{value: 'REL', text: 'Religion & Spirituality'},
+				{value: 'ROM', text: 'Romance'},
+				{value: 'SCI', text: 'Science'},
+				{value: 'SFF', text: 'Science Fiction & Fantasy'},
+				{value: 'SOC', text: 'Society'},
+				{value: 'SPO', text: 'Sports'},
+				{value: 'TEE', text: 'Teen'},
+				{value: 'TRA', text: 'Travel'}
+			]},
+			{type: 'checkbox', name: 'noelectronic', text: 'Block electronic?', label: ' '},
+			{type: 'textbox', name: 'count', size: 40, label: 'Max Count'},
+			{type: 'textbox', name: 'zinio', size: 40, label: 'Zinio Count'},
+			{type: 'label', text: '(To use Zinio you must also configure your Zinio account information with Wowbrary)'}
+		]
+	},
+	{
+		title: 'Size',
+		type: 'form',
+		items: [
+			{type: 'textbox', name: 'height', size: 40, label: 'Height'},
+			{type: 'textbox', name: 'width', size: 40, label: 'Max Width'},
+			{type: 'textbox', name: 'container', size: 40, label: 'Selector'},
+			{type: 'label', text: '(If selector is set, the widget will be resized to match the size of the selected element.)'}
+		]
+	},
+	{
+		title: 'Appearance',
+		type: 'form',
+		items: [
+			{type: 'textbox', name: 'headingtext', size:30, label: 'Heading Text'},
+			{type: 'listbox', name: 'imagesize', label: 'Image Size', values: [
+				{text: 'Small', value: 'S'},
+				{text: 'Medium', value: 'M'},
+				{text: 'Large', value: 'L'}
+			]},
+			{type: 'textbox', name: 'imagescale', size: 40, label: 'Image Scale (%)'},
+			{type: 'textbox', name: 'reflectionsize', size: 40, label: 'Reflection Size (%)'},
+			{type: 'textbox', name: 'spacing', size: 40, label: 'Spacing'}
+		]
+	},
+	{
+		title: 'Motion',
+		type: 'form',
+		items: [
+			{type: 'listbox', name: 'motion', label: 'Motion', values: [
+				{text: 'One at a time', value: 'one-at-a-time'},
+				{text: 'Continuous', value: 'continuous'},
+				{text: 'All at once', value: 'all-at-once'},
+				{text: 'Morph', value: 'morph'}
+			]},
+			{type: 'checkbox', name: 'switchonlyonclick', text: 'Switch only on click?', label: ' '},
+			{type: 'textbox', name: 'switchtime', size: 40, label: 'Switch Time (ms)'},
+			{type: 'textbox', name: 'pause', size: 40, label: 'Pause Time (ms)'}
+		]
+	},
+	{
+		title: 'Buttons',
+		type: 'form',
+		items: [
+			{type: 'textbox', name: 'buttonheight', size: 40, label: 'Button Height'},
+			{type: 'listbox', name: 'buttonstyle', label: 'Button Style', values: [
+				{text: 'Classic', value: 'classic'},
+				{text: 'Modern', value: 'modern'}
+			]},
+			{type: 'textbox', name: 'buttoncolor', size: 40, label: 'Background Color'},
+			{type: 'textbox', name: 'buttontextcolor', size: 40, label: 'Text Color'},
+			{type: 'textbox', name: 'buttonsymbolcolor', size: 40, label: 'Symbol Color'},
+			{type: 'checkbox', name: 'reverse', text: 'Reverse arrow meanings', label: ' '},
+			{type: 'checkbox', name: 'sidearrows', text: 'Put arrow buttons on the side rather than the bottom?', label: ' '}
+		]
+	},
+	{
+		title: 'Style (Advanced)',
+		type: 'form',
+		items: [
+			{type: 'textbox', name: 'borderstyle', size: 40, label: 'Border Style'},
+			{type: 'textbox', name: 'background', size: 40, label: 'Background Color'},
+			{type: 'textbox', name: 'headingstyle', size: 40, label: 'Heading Style', multiline: 'True', minHeight: 60},
+			{type: 'textbox', name: 'titlestyle', size: 40, label: 'Title Style', multiline: 'True', minHeight: 60},
+			{type: 'checkbox', name: 'notitles', text: 'Don’t Use Titles', label: ' '}
+		]
+	},
+	{
+		title: 'Other (advanced)',
+		type: 'form',
+		items: [
+			{type: 'textbox', name: 'target', size: 40, label: 'Link Target'},
+			{type: 'checkbox', name: 'captionflip', text: 'Use screenreader-friendly title attributes', label: ' '},
+			{type: 'checkbox', name: 'debug', text: 'Display debug info', label: ' '},
+			{type: 'checkbox', name: 'iframe', text: 'Put widget in iframe', label: ' '}
+		]
+	}
+];
